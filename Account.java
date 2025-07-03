93e6847c82f2b1aebe9f2896e1c43a2d5362c7f2
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-03 17:07:46 +0300

    Abstraction & Interfaces

--- Account.java
+++ Account.java
@@ -1,9 +1,15 @@
-public class Account {
-    public int accountNo;
-    public String accountName;
-    public double balance;
+abstract public class Account {
+    /*
+     * abstract: This means the class cannot be instantiated directly,
+     * only extended by subclasses
+     */
 
-    public void deposit(double amount) {
-        balance = balance + amount;
+    protected int id; // accessible by subclasses
+    protected String name; // accessible by subclasses
+    public DataSource myData; // Only accessible within Account class
+
+    public void performOperation(DataSource myData) { // takes a DataSource object and executes it
+        this.myData = myData;
+        myData.execute();
     }
 }
